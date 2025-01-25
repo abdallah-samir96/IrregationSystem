1eac7f36df29337094761acc49b6e416a1b63cb0
Âbdallah Šamir <abdallahsameer22@gmail.com>
2025-01-25 18:48:26 +0200

    adding seeds data into the plot table

---
+++ src/main/resources/db/changelog/changelogs/seeds/seed-001.sql
@@ -0,0 +1,6 @@
+insert into plot(name, description, irrigation_period_in_hours)
+values
+    ('plot 001', 'simple description about 001 plot', 100),
+    ('plot 002', 'simple description about 002 plot', 120),
+    ('plot 003', 'simple description about 003 plot', 118),
+    ('plot 004', 'simple description about 004 plot', 125);
